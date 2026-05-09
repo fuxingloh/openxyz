069de4351a4628531535904c0ce814a3a29f184a
Fuxing <4266087+fuxingloh@users.noreply.github.com>
2026-05-10 02:12:13 +0700

    to string first

--- templates/openbrain/drives/brain.ts
+++ templates/openbrain/drives/brain.ts
@@ -2,11 +2,11 @@ import { GitHubDrive } from "@openxyz-provider/github/drive";
 import { env } from "openxyz/env";
 
 export default new GitHubDrive({
-  owner: env.BRAIN_GH_OWNER.describe("GitHub owner (user or org) of the repo backing this brain"),
-  repo: env.BRAIN_GH_REPO.describe("GitHub repo name backing this brain"),
+  owner: env.BRAIN_GH_OWNER.describe("GitHub owner (user or org) of the repo backing this brain").toString(),
+  repo: env.BRAIN_GH_REPO.describe("GitHub repo name backing this brain").toString(),
   branch: "main",
   permission: "read-write",
   token: env.BRAIN_GH_TOKEN.describe(
     "GitHub token with read+write access to the brain repo (fine-grained PAT or App installation token)",
-  ),
+  ).toString(),
 });
